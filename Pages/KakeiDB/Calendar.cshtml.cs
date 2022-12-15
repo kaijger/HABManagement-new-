2e0cbbb01a6bcbc154d6574039d23008dfe37fa6
jun12012001 <119028267+jun12012001@users.noreply.github.com>
2022-12-15 13:46:51 +0900

    カレンダー作成

---
+++ Pages/KakeiDB/Calendar.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HABManagement.Data;
+using HABManagement.Models;
+
+namespace HABManagement.Pages.KakeiDB
+{
+    public class testModel : PageModel
+    {
+        private readonly HABManagement.Data.HABManagementContext _context;
+
+        public testModel(HABManagement.Data.HABManagementContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Kakei Kakei { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Kakei.Add(Kakei);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
