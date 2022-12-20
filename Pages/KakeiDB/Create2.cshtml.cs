d71b81ebb218c11ba728d9050718877b64f87ae0
jun12012001 <119028267+jun12012001@users.noreply.github.com>
2022-12-20 10:49:51 +0900

    支出収入分け

--- Pages/KakeiDB/Create2.cshtml.cs
+++ Pages/KakeiDB/Create2.cshtml.cs
@@ -10,11 +10,11 @@ using HABManagement.Models;
 
 namespace HABManagement.Pages.KakeiDB
 {
-    public class CreateModel : PageModel
+    public class Create2Model : PageModel
     {
         private readonly HABManagement.Data.HABManagementContext _context;
 
-        public CreateModel(HABManagement.Data.HABManagementContext context)
+        public Create2Model(HABManagement.Data.HABManagementContext context)
         {
             _context = context;
         }
@@ -26,25 +26,24 @@ namespace HABManagement.Pages.KakeiDB
 
         [BindProperty]
         public Kakei Kakei { get; set; }
-        
+
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return Page();
             }
-			if (Kakei.Category == "null")
-			{
-				Kakei.Category = "";
-			}
-			if (Kakei.Balance == "null")
-			{
-				Kakei.Balance = "支出";
-			}
-
-			_context.Kakei.Add(Kakei);
+            if (Kakei.Category == "null")
+            {
+                Kakei.Category = "";
+            }
+            if (Kakei.Balance == "null")
+            {
+                Kakei.Balance = "収入";
+            }
+            _context.Kakei.Add(Kakei);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
