84e67a76857553d8eba7b30782e6be2bd6b39405
Akari00 <a.kaihatsu.sw000@gmail.com>
2022-12-23 18:19:28 +0900

    日付初期値設定(家計簿)

--- Pages/KakeiDB/Create.cshtml.cs
+++ Pages/KakeiDB/Create.cshtml.cs
@@ -7,20 +7,36 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using HABManagement.Data;
 using HABManagement.Models;
+using Microsoft.Extensions.Logging;
 
 namespace HABManagement.Pages.KakeiDB
 {
     public class CreateModel : PageModel
     {
         private readonly HABManagement.Data.HABManagementContext _context;
-
-        public CreateModel(HABManagement.Data.HABManagementContext context)
+        private readonly ILogger<IndexModel> _logger;
+        public CreateModel(HABManagement.Data.HABManagementContext context, ILogger<IndexModel> logger)
         {
             _context = context;
+            _logger = logger;
         }
+
+        public const string SessionKeyName = "_Name";
 
         public IActionResult OnGet()
         {
+            string yr = DateTime.Today.Year.ToString();
+            string mn = DateTime.Today.Month.ToString();
+            string dt = DateTime.Today.Day.ToString();
+            string datenow = string.Format("{0}-{1}-{2}", yr, mn, dt);
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(SessionKeyName)))
+            {
+                HttpContext.Session.SetString(SessionKeyName, datenow);
+
+            }
+            var name = HttpContext.Session.GetString(SessionKeyName);
+
+            _logger.LogInformation("Session Name: {Name}", name);
             return Page();
         }
 
@@ -31,7 +47,8 @@ namespace HABManagement.Pages.KakeiDB
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid)
+            
+            if (!ModelState.IsValid)
             {
                 return Page();
             }
