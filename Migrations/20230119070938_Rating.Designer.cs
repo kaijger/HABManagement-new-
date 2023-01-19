2207c81f6c91a538df6a74f68bb90c9f9dc1660f
Akari00 <a.kaihatsu.sw000@gmail.com>
2023-01-20 00:09:12 +0900

    買物リスト優先度設定

--- Migrations/20230119070938_Rating.Designer.cs
+++ Migrations/20230119070938_Rating.Designer.cs
@@ -4,6 +4,7 @@ using HABManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HABManagement.Migrations
 {
     [DbContext(typeof(HABManagementContext))]
-    partial class HABManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20230119070938_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/SList.cs
+++ Models/SList.cs
@@ -5,5 +5,6 @@
         public int ID { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Memo { get; set; } = string.Empty;
+        public int? Priority { get; set; }
     }
 }
